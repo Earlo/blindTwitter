1a87e43fdffdbce2df8d33f3a674e75952e7f497
Jere Vaara <jerevaara@gmail.com>
2023-07-12 15:44:21 +0300

    add shuf

--- content.js
+++ content.js
@@ -40,6 +40,24 @@ function throttle(func, limit) {
     };
 }
 
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
 let originalData = {};
 // Function to replace profile images, names and handles
 function replaceElements() {
